fdc12edae46e9ee558101d4e9cd6ab9c83cbda80
JLLivisaca <jllivisaca@users.noreply.github.com>
2023-11-08 09:52:44 -0500

    Update Diseno 2 k - Velo - Profundiddad  -1.R

--- Diseno 2 k - Velo - Profundiddad  -1.R
+++ Diseno 2 k - Velo - Profundiddad  -1.R
@@ -5,6 +5,11 @@ Datos_2k <- read_excel("GitHub/estadistica-inferencia/DOE-UCuenca/dATOS-2k EJEMP
 Datos_2k$Profundidad <-  as.factor(Datos_2k$Profundidad)
 Datos_2k$Velocidad <- as.factor(Datos_2k$Velocidad)
 
+# Colocar nombres
+
+attach(Datos_2k)
+names(Datos_2k)
 # Modelo ANOVA
 
-
+Modelo_2k <-  aov(Acabado ~ Profundidad*Velocidad)
+summary(Modelo_2k)
